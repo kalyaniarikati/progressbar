814dbeae405810740037754626d3fab18dc09c10
kalyaniarikati <63218307+kalyaniarikati@users.noreply.github.com>
2023-03-27 14:46:38 +1100

    tests

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.css';
-import Button from "react-bootstrap/Button";
-import ProgressBar from "react-bootstrap/ProgressBar";
-import Dropdown from 'react-bootstrap/Dropdown';
+import Progress from '../src/components/Progressbar/Progress';
+import items from '../src/data';
 
 function App() {
   const [barname, setBarname] = React.useState("");
   const [update, setUpdate] = React.useState(false);
   const [value, setValue] = React.useState(0);
-  const items = [
-    {name:"Progress Bar1",
-    value:10,
-    color:"success"}, 
-    {name:"Progress Bar2",
-    value:25,
-    color:"info"}, 
-    {name:"Progress Bar3",
-    value:50,
-    color:"warning"}];
   const [item, setItem] = React.useState(items);
  
   React.useEffect(() => {
@@ -26,8 +15,6 @@ function App() {
       const items = item.map((newitem, i) =>  {
         if(newitem.name === barname){
           newitem.value = newitem.value+value
-        if (newitem.value >= 100) 
-           newitem.value = 100;
         if (newitem.value < 0) 
            newitem.value = 0;
         }           
@@ -40,40 +27,14 @@ function App() {
 }, [update]);
 
   return (
-    <div >
-    <h1>
-      Progress Bar
-      <div className="component">
-      {item.map((it, i) => (
-        <div style={{ display: 'block',
-        width: 700, padding: 30 }}>
-          {it.name}: {parseInt(it.value)} %
-        <ProgressBar variant={it.color} now={it.value} label= {`${it.value}%`} />
-        </div>  
-        ))}
-        <Dropdown>
-          <Dropdown.Toggle variant="secondary" id="dropdown-basic" >
-          {barname? barname: "Select Progress Bar"}
-          </Dropdown.Toggle>
-          <Dropdown.Menu>
-        {item.map((it, i) => (
-          <Dropdown.Item
-            key={i}
-            as="button"
-            onClick={() => setBarname(it.name)}
-          >
-            {it.name}
-          </Dropdown.Item>
-        ))}
-      </Dropdown.Menu>
-    </Dropdown>
-            <Button variant="primary" onClick={() => (setUpdate(true), setValue(-(-10)))} >+10</Button>{' '}
-           <Button variant="primary" onClick={() => (setUpdate(true), setValue(-(-25)))} value="+25">+25</Button>{' '}
-           <Button variant="primary" onClick={() => (setUpdate(true), setValue(-10))} value="-10">-10</Button>{' '}
-           <Button variant="primary"  onClick={() => (setUpdate(true), setValue(-25))}value="-25">-25</Button>{' '}
-      </div>
-    </h1>
-  </div>
+    <div>
+    <Progress item={item}
+    setBarname={setBarname}
+    setUpdate={setUpdate}
+    setValue={setValue}
+    barname={barname}
+    />
+    </div>
   );
 }
 
